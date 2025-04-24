e35c7fa1f090057ea468b3304f9d87d0a049477f
Mateo Malpassi <mateo.malpass@mi.unc.edu.ar>
2025-04-24 17:58:59 -0300

    Added the pyshorteners option to shorten the URL

--- url_shortener/chopURL/views.py
+++ url_shortener/chopURL/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import shortURL
 import string, random
+import pyshorteners
 
 def generate_short_code(length=6):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 def chop_url(request):
     if request.method == 'POST':
-        url = request.POST['url']
-        code = generate_short_code()
-        short_url = shortURL.objects.create(original_url=url, short_code=code)
-        return render(request, 'result.html', {'short_url': short_url})
+        url = request.POST.get('url')
+        shortener_option = request.POST.get('chopurl_option')
+
+        if shortener_option == 'chopservice':
+            code = generate_short_code()
+            chop_short_url = shortURL.objects.create(original_url=url, short_code=code)
+            return render(request, 'result.html', {
+                'chop_short_url': chop_short_url,
+                'pyservice_short_url': None,
+            })
+        
+        elif shortener_option == 'pyservice':
+            s = pyshorteners.Shortener()
+            try:
+                pyservice_short_url = s.tinyurl.short(url)
+            except Exception as e:
+                pyservice_short_url = None
+            return render(request, 'result.html', {
+                'chop_short_url': None,
+                'pyservice_short_url': pyservice_short_url,
+            })
     
     # If method == GET:
     return render(request, 'index.html')
